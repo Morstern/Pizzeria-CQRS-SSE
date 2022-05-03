495392775d38d881aba5a667d1c955603fc86785
Kamil Zielinski <kamilziel@tlen.pl>
2022-05-03 12:57:23 +0200

    [ZIELINSKI] Added missing annotation

--- service/src/main/java/pl/morstern/PizzaCQRS/service/serversentevents/utils/UUIDUtil.java
+++ service/src/main/java/pl/morstern/PizzaCQRS/service/serversentevents/utils/UUIDUtil.java
@@ -1,7 +1,10 @@
 package pl.morstern.PizzaCQRS.service.serversentevents.utils;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class UUIDUtil {
     public String generate(){
         return UUID.randomUUID().toString();
